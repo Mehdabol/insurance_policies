cbd02b7d1a871a7bcf1dffee3a7f343f83794ced
Mehdabol <45933250+Mehdabol@users.noreply.github.com>
2020-01-16 10:48:38 +0330

    create

--- src/app/module/create-user/component/add-create-user/add-create-user.component.ts
+++ src/app/module/create-user/component/add-create-user/add-create-user.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormValidateService} from '../../../../core/services/Form-Validate.service';
+import {AlertService} from '../../../../core/services/alert.service';
 
 @Component({
   selector: 'app-add-create-user',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-create-user.component.scss']
 })
 export class AddCreateUserComponent implements OnInit {
+  brithDate = '';
+  FormatDate = {format: 'YYYY/MM/DD'};
 
-  constructor() { }
+  constructor(private errorService: FormValidateService,
+              private alertService: AlertService,
+              private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  onSubmitForm(form) {
+    if (form.valid) {
+      this.submittedForm(form.value);
+    } else {
+      this.errorService.generateArray(form);
+    }
+  }
+
+  submittedForm(value) {
+
+  }
+
+  onBack() {
+    this.router.navigate(['/pages/create-user']);
+  }
 }
